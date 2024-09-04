52e90a29126fe878c76e68a72222e18eca6a8038
thanchayawik <thanchayawik@gmail.com>
2024-09-04 15:46:22 +0700

    write messages to firestore

--- firebase-get-to-know-flutter/step_02/lib/app_state.dart
+++ firebase-get-to-know-flutter/step_02/lib/app_state.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart'
     hide EmailAuthProvider, PhoneAuthProvider;
 import 'package:firebase_core/firebase_core.dart';
@@ -9,6 +10,21 @@ import 'firebase_options.dart';
 class ApplicationState extends ChangeNotifier {
   ApplicationState() {
     init();
+  }
+
+  Future<DocumentReference> addMessageToGuestBook(String message) {
+    if (!_loggedIn) {
+      throw Exception('Must be logged in');
+    }
+
+    return FirebaseFirestore.instance
+        .collection('guestbook')
+        .add(<String, dynamic>{
+      'text': message,
+      'timestamp': DateTime.now().millisecondsSinceEpoch,
+      'name': FirebaseAuth.instance.currentUser!.displayName,
+      'userId': FirebaseAuth.instance.currentUser!.uid,
+    });
   }
 
   bool _loggedIn = false;
--- firebase-get-to-know-flutter/step_02/lib/home_page.dart
+++ firebase-get-to-know-flutter/step_02/lib/home_page.dart
@@ -6,6 +6,7 @@ import 'package:flutter/material.dart'; // new
 import 'package:provider/provider.dart'; // new
 
 import 'app_state.dart'; // new
+import 'guest_book.dart';
 import 'src/authentication.dart'; // new
 import 'src/widgets.dart';
 
@@ -43,6 +44,20 @@ class HomePage extends StatelessWidget {
           const Header("What we'll be doing"),
           const Paragraph(
             'Join us for a day full of Firebase Workshops and Pizza!',
+          ),
+          Consumer<ApplicationState>(
+            builder: (context, appState, _) => Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                if (appState.loggedIn) ...[
+                  const Header('Discussion'),
+                  GuestBook(
+                    addMessage: (message) =>
+                        appState.addMessageToGuestBook(message),
+                  ),
+                ],
+              ],
+            ),
           ),
         ],
       ),
